9e7849ed1797e78aaec4833bb6c77cacb146cd41
lumen_xie <lumen_xie@163.com>
2019-10-02 15:08:20 +0800

    change demo file_name

--- demo_01/24_函数传值与传址.c
+++ demo_01/24_函数传值与传址.c
@@ -21,9 +21,16 @@ int main(void)
     int *p1 = &a;
     printf("a = %p\n",&a);
     printf("p1 = %p\n",p1);
-//    fun(p1);
- //   a = 0060FEF8 p1 = 0060FEF8 p = 0060FEF8 p = 00000000 p1 = 0060FEF8
-    fun2(&p1);
+	fun(p1);//传递的是p1的值，也就是a的地址，a是一个int型变量；
+//  a = 0060FEF8 p1 = 0060FEF8 p = 0060FEF8 p = 00000000 p1 = 0060FEF8
+    fun2(&p1);//传递的是p1的地址，p1是一个指针；
+/*
+	a = 0060FEFC
+	p1 = 0060FEFC
+	p = 0060FEFC
+	p = 00000000
+	p1 = 00000000
+*/
     printf("p1 = %p\n",p1);
 
     return 0;
